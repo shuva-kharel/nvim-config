d7aacca06fae2397bc2e126a9618c7cf3ef03015
Shuva Kharel <shuvakharel007@gmail.com>
2025-01-15 22:28:06 +0545

    Basic Config

--- init.lua
+++ init.lua
@@ -27,7 +27,8 @@ local plugins = {
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' }
-  }
+  },
+	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
 }
 local opts = {}
 
@@ -35,6 +36,17 @@ require("lazy").setup(plugins, opts)
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+local config =  require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {
+    "lua", "javascript", "python", "cpp", "c", 
+    "html", "css", "markdown", "bash"
+  },
+  sync_install = false,
+  highlight = { enable = true },
+  indent = { enable = true },  
+})
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
