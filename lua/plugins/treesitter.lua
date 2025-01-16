f08f8c2bec917514d6e1ae9571960121b125d963
Shuva Kharel <shuvakharel007@gmail.com>
2025-01-16 23:10:14 +0545

    Config Organization

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-treesitter/nvim-treesitter", 
+  build = ":TSUpdate",
+  config = function()
+    local ts_config = require("nvim-treesitter.configs")
+
+    ts_config.setup({
+      ensure_installed = {
+        "lua", "javascript", "python", "cpp", "c", 
+        "html", "css", "markdown", "bash"
+      },
+      sync_install = false,
+      highlight = { 
+        enable = true,  -- Enable Treesitter-based highlighting
+      },
+      indent = { 
+        enable = true,  -- Enable Treesitter-based indentation
+      },
+      -- Optionally enable other features like folding, rainbow brackets, etc.
+      -- textobjects = { enable = true },   -- Enable text objects support
+      -- rainbow = { enable = true },       -- Enable rainbow parentheses
+    })
+  end
+}
+
