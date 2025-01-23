4068acab9d4e229c7660b09bf7495571f2264870
Shuva Kharel <shuvakharel007@gmail.com>
2025-01-23 23:35:27 +0545

    LSP Configured

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls", "clangd", "pyright", "jinja_lsp", "ts_ls",
+          "html", "cssls", "tailwindcss", "eslint", "bashls"
+        }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      
+      lspconfig.lua_ls.setup({})
+      lspconfig.clangd.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.jinja_lsp.setup({})
+      lspconfig.ts_ls.setup({})
+      lspconfig.html.setup({})
+      lspconfig.cssls.setup({})
+      lspconfig.tailwindcss.setup({})
+      lspconfig.eslint.setup({})
+      lspconfig.bashls.setup({})
+
+      -- Set keymaps after LSP is configured
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+    end
+  }
+}
+
